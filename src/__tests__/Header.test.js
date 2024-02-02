1e69546c2d72099752965515b82533e173c40551
norahkibathi <norah.njeri@student.moringaschool.com>
2024-02-01 22:52:10 -0500

    completed assignment

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,5 +1,3 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
 import React from "react";
 
 import Header from "../components/Header";
@@ -9,3 +7,10 @@ test("displays the text 'hello from the Header!'", () => {
 
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
+import React from "react";
+
+function Header(props) {
+  return <h1>hello from the Header!</h1>;
+}
+
+export default Header;
